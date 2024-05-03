ab0a15ae9c98744bcd9308cec7c4473a9b73398b
DyonneCunha <bydyonne@gmail.com>
2024-05-03 15:56:09 -0300

    first commit

---
+++ src/br/com/alura/desafio/Principal.java
@@ -0,0 +1,7 @@
+package br.com.alura.desafio;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/com/alura/desafio/connection/api/ExchangerateApi.java
@@ -0,0 +1,4 @@
+package br.com.alura.desafio.connection.api;
+
+public class ExchangerateApi {
+}
